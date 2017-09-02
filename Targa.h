4c6fe387ecf98735ece0eec12d10d14fc1084a1f
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:36:22 +0100

    Initial import

---
+++ Targa.h
@@ -0,0 +1,29 @@
+#ifndef _TARGA_H
+#define _TARGA_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Targa10[];
+    extern const uint8_t Targa11[];
+    extern const uint8_t Targa12[];
+    extern const uint8_t Targa13[];
+    extern const uint8_t Targa14[];
+    extern const uint8_t Targa15[];
+    extern const uint8_t Targa16[];
+    extern const uint8_t Targa17[];
+    extern const uint8_t Targa18[];
+    extern const uint8_t Targa19[];
+    extern const uint8_t Targa20[];
+    extern const uint8_t Targa21[];
+    extern const uint8_t Targa22[];
+    extern const uint8_t Targa23[];
+    extern const uint8_t Targa24[];
+    extern const uint8_t Targa25[];
+    extern const uint8_t Targa26[];
+    extern const uint8_t Targa27[];
+    extern const uint8_t Targa28[];
+    extern const uint8_t Targa29[];
+    extern const uint8_t Targa30[];
+};
+#endif
